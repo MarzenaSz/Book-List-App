c1821af149ec47ef58f9e01f8067cd7ef0e929a8
Marzena <marzena_szopinska@hotmail.com>
2018-02-13 19:13:57 +0100

    created books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+
+export default function() {
+    return [
+        { title: "JavaScript: The good parts" },
+        { title: "Harry Potter" },
+        { title: "The Dark Tower" },
+        { title: "Better Now Than Never" }
+    ]
+}
